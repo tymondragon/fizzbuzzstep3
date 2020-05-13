7a682a4b9e66a3c48393eb7fa016327dfd6612a6
Ty Mondragon <tymondragon@gmail.com>
2020-05-12 19:44:14 -0600

    App prints out fizz buzz list and counts

--- src/main/java/com/fizzbuzz/App.java
+++ src/main/java/com/fizzbuzz/App.java
@@ -16,7 +16,12 @@ public class App
         final int length = scanner.nextInt();
         scanner.close();
         final String result = fizzBuzz.buildString(length);
-        System.out.print(result);
+        System.out.println(result);
+        System.out.println("fizz: " + fizzBuzz.getFizzCount());
+        System.out.println("buzz: " + fizzBuzz.getBuzzCount());
+        System.out.println("fizzBuzz: " + fizzBuzz.getFizzBuzzCount());
+        System.out.println("lucky: " + fizzBuzz.getLuckyCount());
+        System.out.println("integer: " + fizzBuzz.getIntegerCount());
     }
 }
 
